3065459ed53f6aee3c948afe1c0aa52ce297c66a
yuanqing <desk@yuanqing.sg>
2015-05-29 16:21:18 +0800

    Add `async`

--- app/async.js
+++ app/async.js
@@ -1,11 +1,25 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async : function(value) {
 
+  async : function(value) {
+    return {
+      then: function(cb) {
+        cb(value); // uh, just call the `cb` with `value`
+      }
+    };
   },
 
   manipulateRemoteData : function(url) {
-
+    var then = function(cb) {
+      $.get(url, function(data) {
+        var names = _.pluck(data.people, 'name');
+        cb(names.sort());
+      });
+    };
+    return {
+      then: then
+    };
   }
+
 };
