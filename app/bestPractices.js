7d1769b02273e8f639a472a3024ae49ef00c0856
yuanqing <desk@yuanqing.sg>
2015-05-29 16:25:16 +0800

    Add `bestPractices`

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -7,29 +7,29 @@ exports = (typeof window === 'undefined') ? global : window;
  */
 
 exports.bestPracticesAnswers = {
+
   globals : function() {
-    myObject = {
+    var myObject = {
       name : 'Jory'
     };
-
     return myObject;
   },
 
   functions : function(flag) {
-    if (flag) {
-      function getValue() { return 'a'; }
-    } else {
-      function getValue() { return 'b'; }
-    }
-
+    var getValue = flag ? function() {
+      return 'a';
+    } : function() {
+      return 'b';
+    };
     return getValue();
   },
 
   parseInt : function(num) {
-    return parseInt(num);
+    return parseInt(num, 10);
   },
 
   identity : function(val1, val2) {
-
+    return val1 === val2;
   }
+
 };
