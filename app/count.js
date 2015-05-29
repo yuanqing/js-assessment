1fdc31c6c820c84e44bd6a14afb757125774ddc0
yuanqing <desk@yuanqing.sg>
2015-05-29 16:32:56 +0800

    Add `count`

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-
+    var id = setInterval(function() {
+      console.log(start++); // log the number, then increment
+      if (start > end) {
+        cancel(); // cancel when done
+      }
+    }, 100);
+    var cancel = function() {
+      clearInterval(id);
+    };
+    return {
+      cancel: cancel
+    };
   }
 };
