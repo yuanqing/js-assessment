f0c92a8e568cf6b2234d760b48ba42da4f928514
yuanqing <desk@yuanqing.sg>
2015-05-29 18:37:41 +0800

    Add `objects`

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    var i = -1;
+    var result = [];
+    var key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        result.push(key + ': ' + obj[key]);
+      }
+    }
+    return result;
   }
 };
