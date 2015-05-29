3a8c42ee0c993aef391fff286c450f47aab4a17a
yuanqing <desk@yuanqing.sg>
2015-05-29 17:14:49 +0800

    Add `modules`

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
-
+    var obj = {
+      greeting: str1,
+      name: str2,
+      sayIt: function() {
+        return obj.greeting + ', ' + obj.name;
+      }
+    };
+    return obj;
   }
 };
