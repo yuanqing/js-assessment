e866c8ba28e582ca0d27e071757677aaa19bd56e
yuanqing <desk@yuanqing.sg>
2015-05-29 17:11:39 +0800

    Add `logicalOperators`

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -1,11 +1,13 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.logicalOperatorsAnswers = {
-  or : function(a, b) {
 
+  or : function(a, b) {
+    return a || b;
   },
 
   and : function(a, b) {
-
+    return a && b;
   }
+
 };
