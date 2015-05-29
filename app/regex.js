10b17f4eeaa41154a86c1640a538ae177d41a503
yuanqing <desk@yuanqing.sg>
2015-05-29 20:04:05 +0800

    Add `regex`

--- app/regex.js
+++ app/regex.js
@@ -1,26 +1,48 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
-  containsNumber : function(str) {
 
-  },
+  containsNumber : (function() {
+    var regex = /\d/;
+    return function(str) {
+      return regex.test(str);
+    };
+  })(),
 
-  containsRepeatingLetter : function(str) {
+  containsRepeatingLetter : (function() {
+    var regex = /([a-zA-Z])\1/;
+    return function(str) {
+      return regex.test(str);
+    };
+  })(),
 
-  },
+  endsWithVowel : (function() {
+    var regex = /[aeiou]$/i;
+    return function(str) {
+      return regex.test(str);
+    };
+  })(),
 
-  endsWithVowel : function(str) {
+  captureThreeNumbers : (function() {
+    var regex = /(\d{3})/;
+    return function(str) {
+      var matches = str.match(regex);
+      return matches ? matches[1] : false;
+    };
+  })(),
 
-  },
+  matchesPattern : (function() {
+    var regex = /^\d{3}-\d{3}-\d{4}$/;
+    return function(str) {
+      return regex.test(str);
+    };
+  })(),
 
-  captureThreeNumbers : function(str) {
+  isUSD : (function() {
+    var regex = /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/;
+    return function(str) {
+      return regex.test(str);
+    };
+  })(),
 
-  },
-
-  matchesPattern : function(str) {
-
-  },
-  isUSD : function(str) {
-
-  }
 };
